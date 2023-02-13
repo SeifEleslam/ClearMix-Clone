bf1d252c84e23546ec295478aff1226835b10ca2
SeifEleslam <seifgouda98@gmail.com>
2023-02-14 01:27:23 +0200

    smoothe menu animation

--- src/components/navbar/MenuBody.tsx
+++ src/components/navbar/MenuBody.tsx
@@ -10,8 +10,7 @@ export const MenuBody = ({ open }: { open: boolean }) => {
           className="fixed w-full h-full lg:hidden"
           initial={{ opacity: 0, scale: 1 }}
           animate={{ opacity: 1, scale: 1 }}
-          exit={{ opacity: 0, scale: 1.2 }}
-          transition={{ duration: 0.1 }}
+          exit={{ opacity: 0, scale: 1.2, transition: { duration: 0.3 } }}
         >
           <div className="w-full h-full p-2 overflow-scroll">
             <div className="flex relative z-10 flex-col w-full h-full min-h-[500px] ">
@@ -36,8 +35,8 @@ export const MenuBody = ({ open }: { open: boolean }) => {
               <m.div
                 className="flex flex-col md:flex-row justify-center items-center h-fit p-4 md:space-x-10 md:space-y-0 space-y-5"
                 initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 0.4 }}
+                animate={{ opacity: 1, transition: { delay: 0.4 } }}
+                exit={{ opacity: 0 }}
               >
                 <LoginButton widthClass="w-full p-6">LOGIN</LoginButton>
                 <SchButton widthClass="w-full">SCHEDULE A CALL</SchButton>
@@ -48,7 +47,7 @@ export const MenuBody = ({ open }: { open: boolean }) => {
           <m.div
             className="bg-bgprim absolute top-[-10rem] right-[-10rem] z-0 w-[20rem] h-[20rem] rounded-full"
             initial={{ scale: 1 }}
-            animate={{ scale: 8 }}
+            animate={{ scale: 9 }}
             transition={{ duration: 0.4 }}
           ></m.div>
         </m.div>
@@ -71,6 +70,7 @@ const Item = ({ children, i }: { children: React.ReactNode; i: number }) => {
         y: 0,
         transition: { duration: 0.3, delay: 0.1 * (i + 1), type: "tween" },
       }}
+      exit={{ opacity: 0 }}
     >
       {children}
     </m.a>
