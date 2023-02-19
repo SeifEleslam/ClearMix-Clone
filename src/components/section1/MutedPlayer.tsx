e26f5e2430cc034db3be17e7f635c3c1309ca5cd
SeifEleslam <seifgouda98@gmail.com>
2023-02-19 19:00:53 +0200

    testing sync in section4

--- src/components/section1/MutedPlayer.tsx
+++ src/components/section1/MutedPlayer.tsx
@@ -40,3 +40,61 @@ export default function MutedPlayer({
     </div>
   );
 }
+
+// export function SyncPlayers({
+//   url,
+//   newClasses,
+//   style,
+// }: {
+//   url: string;
+//   newClasses: string;
+//   style?: any;
+// }) {
+//   const ref = useRef(null);
+//   const [view, setView] = useState<{ first: boolean; secound: boolean }>({
+//     first: false,
+//     secound: false,
+//   });
+//   const render = useInView(ref, { once: true });
+//   const play = useInView(ref);
+
+//   useEffect(() => {
+//     console.log(render);
+//   }, [render]);
+//   return (
+//     <div ref={ref} style={style} className={newClasses + " overflow-clip"}>
+//       <div className="absolute h-full w-[50%] overflow-x-clip">
+//         <div className="absolute w-full h-full top-0 left-0 bg-bgprim/25"></div>
+//         {render && (
+//           <ReactPlayer
+//             url="https://stream.mux.com/T3DIas3z00UdwJmADar00eKya2BLxRqeQ14z4Fpi70242E.m3u8"
+//             playing={play && view.first && view.secound}
+//             loop={true}
+//             muted={true}
+//             width="100%"
+//             height="auto"
+//             onReady={() => {
+//               setView((prv) => ({ ...prv, first: true }));
+//             }}
+//           />
+//         )}
+//       </div>
+//       <div className="w-full">
+//         <div className="absolute w-full h-full top-0 left-0 bg-bgprim/25"></div>
+//         {render && (
+//           <ReactPlayer
+//             url="https://stream.mux.com/Q9k700YSBu014nlXJwZQIHmk9NWlswG3qGf4uGw5C4Cr00.m3u8"
+//             playing={play && view.first && view.secound}
+//             loop={true}
+//             muted={true}
+//             width="100%"
+//             height="auto"
+//             onReady={() => {
+//               setView((prv) => ({ ...prv, secound: true }));
+//             }}
+//           />
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
--- src/components/section4/Comparance.tsx
+++ src/components/section4/Comparance.tsx
@@ -1,24 +1,55 @@
-import { lazy, Suspense } from "react";
+import { useInView } from "framer-motion";
+import dynamic from "next/dynamic";
+import { useRef, useState } from "react";
 import useMeasure from "react-use-measure";
 
-const MutedPlayer = lazy(() => import("../section1/MutedPlayer"));
+const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
 
 export const Comparance = () => {
-  const [ref, { width }] = useMeasure();
+  const [widthRef, { width }] = useMeasure();
+  const ref = useRef(null);
+  const [view, setView] = useState<{ first: boolean; secound: boolean }>({
+    first: false,
+    secound: false,
+  });
+  const render = useInView(ref, { once: true });
+  const play = useInView(ref);
   return (
-    <div ref={ref} className="w-full relative h-fit rounded-2xl">
-      <div className="overflow-clip absolute h-full w-[50%]  rounded-2xl">
-        <MutedPlayer
-          url="https://stream.mux.com/T3DIas3z00UdwJmADar00eKya2BLxRqeQ14z4Fpi70242E.m3u8"
-          newClasses={""}
-          style={{ width }}
-        />
+    <div
+      ref={widthRef}
+      className="w-full relative h-fit overflow-clip rounded-2xl"
+    >
+      <div className="absolute h-full w-[50%] overflow-x-clip">
+        <div className="absolute w-full h-full top-0 left-0 bg-bgprim/25"></div>
+        {render && (
+          <ReactPlayer
+            url="https://stream.mux.com/T3DIas3z00UdwJmADar00eKya2BLxRqeQ14z4Fpi70242E.m3u8"
+            playing={play && view.first && view.secound}
+            loop={true}
+            muted={true}
+            width="100%"
+            height="auto"
+            onReady={() => {
+              setView((prv) => ({ ...prv, first: true }));
+            }}
+          />
+        )}
       </div>
-      <div className="w-full overflow-clip rounded-2xl">
-        <MutedPlayer
-          url="https://stream.mux.com/Q9k700YSBu014nlXJwZQIHmk9NWlswG3qGf4uGw5C4Cr00.m3u8"
-          newClasses={""}
-        />
+      <div className="w-full">
+        <div className="absolute w-full h-full top-0 left-0 bg-bgprim/25"></div>
+        {render && (
+          <ReactPlayer
+            url="https://stream.mux.com/Q9k700YSBu014nlXJwZQIHmk9NWlswG3qGf4uGw5C4Cr00.m3u8"
+            playing={play && view.first && view.secound}
+            loop={true}
+            muted={true}
+            width="100%"
+            height="auto"
+            onReady={() => {
+              setView((prv) => ({ ...prv, secound: true }));
+            }}
+          />
+        )}
       </div>
     </div>
   );
