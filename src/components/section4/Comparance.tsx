e6278b315b8a1eb714400df8b0495df2f637c9f1
SeifEleslam <seifgouda98@gmail.com>
2023-02-22 22:27:31 +0200

    minior changes2

--- src/components/section4/Comparance.tsx
+++ src/components/section4/Comparance.tsx
@@ -50,7 +50,7 @@ export const Comparance = ({ width }: { width: number }) => {
         style={{ x: xSmooth }}
         className={`z-10 top-0 absolute bg-bgprim h-full w-[1px]`}
       ></motion.div>
-      <m.button
+      <motion.button
         initial={{ scale: 0 }}
         whileInView={{ scale: 1, transition: { delay: 0.3 } }}
         viewport={{ once: true }}
@@ -88,7 +88,7 @@ export const Comparance = ({ width }: { width: number }) => {
             strokeLinejoin="round"
           ></path>
         </svg>
-      </m.button>
+      </motion.button>
       <motion.div
         ref={ref}
         style={{ width: newWidth }}
